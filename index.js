771f316a1f3a92e3f993f198bf63dd30c979bab4
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-02-26 15:18:34 -0600

    setup  handlebars

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express');
-const fs = require('fs');
-
+const path = require('path');
+const rootDir = require('./util/path');
 const app = express();
+const hbs = require('hbs');
 const port = process.env.PORT || 3000;
+
+const partialsPath = path.join(__dirname, './views/partials');
+
+// Setup handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(partialsPath);
 
 app.use('/users', (req, res, next) => {
   console.log('I am here too');
   res.send('<h1>Extra stuff</h1>');
 });
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Stuff</h1>');
+app.get('/', (req, res, next) => {
   console.log('I am here');
+  res.render('index', {
+    title: 'Delivery App'
+  });
 });
 
 app.listen(port);
