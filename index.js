4f9cfb0c706bfb2a4970d883bb524afdafc3dc59
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-06-12 16:24:42 -0500

    database connection attempt

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
-const pool = require('./db');
+// const pool = require('./db');
+const client = require('./db');
+
+client.connect();
 
 //const rootDir = require('./util/path');
 const app = express();
@@ -11,11 +14,24 @@ const partialsPath = path.join(__dirname, './views/partials');
 const publicDirectoryPath = path.join(__dirname, './public');
 
 app.use(express.static(publicDirectoryPath));
+app.use(express.json()); // req.body
 
 // Setup handlebars
 app.set('view engine', 'hbs');
 hbs.registerPartials(partialsPath);
 
+// DB Routes
+// Add an account
+app.get('/account', async (req, res) => {
+  try {
+    const account = await pool.query('SELECT * FROM account');
+    res.json(account.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// Routes
 app.use('/users', (req, res, next) => {
   console.log('I am here too');
   res.send('<h1>Extra stuff</h1>');
