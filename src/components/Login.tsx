56c2b98ebbc1ea3871468a4fccb2526d2de1498b
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-08-11 22:34:04 -0500

    move project over to React and TypeScript

---
+++ src/components/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Login extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div>
+        <h1>This is from the Log in</h1>
+      </div>
+    );
+  }
+}
