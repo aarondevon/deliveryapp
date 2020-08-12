2bf29e6cce047d49306994b73742300d4d1e16a0
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-08-11 23:32:08 -0500

    move from function to class

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Login />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component<{},{}> {
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <Login/>
+          </header>
+        </div>
+    );
+  }
 
-export default App;
+
+}
