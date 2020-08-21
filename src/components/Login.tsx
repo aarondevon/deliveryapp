c8186a13e6ccde78030426dc1a58f7bc7fe21443
Aaron <10297223+aarondevon@users.noreply.github.com>
2020-08-21 17:40:42 -0500

    begin adding more styles

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -9,13 +9,17 @@ export default class Login extends React.Component<LoginProps, {}> {
   render() {
     return (
       <div>
-          <Link to="/create">Create Account</Link>
+
         <h1>Idag</h1>
         <form>
           <input type="text" />
 
           <button type="submit">Login</button>
         </form>
+          <div className="create-account">
+              <Link to="/create">Create Account</Link>
+          </div>
+
       </div>
     );
   }
