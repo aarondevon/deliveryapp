b00a047f7f8ed71805c3a1b15427f6085e3eb092
Aaron <10297223+aarondevon@users.noreply.github.com>
2020-08-24 10:52:47 -0500

    add styles to login component

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -9,16 +9,30 @@ export default class Login extends React.Component<LoginProps, {}> {
   render() {
     return (
       <div>
+          <h1>Idag</h1>
+        <div className="login-container">
+            <div className="login-form-container">
+                <form id="form" className="form">
+                    <h2 className="login-header">Login</h2>
+                    <div className="login-form-control">
+                        <label>Email</label>
+                        <input type="email" />
+                    </div>
 
-        <h1>Idag</h1>
-        <form>
-          <input type="text" />
+                    <div className="login-form-control">
+                        <label>Password</label>
+                        <input type="password"/>
+                    </div>
 
-          <button type="submit">Login</button>
-        </form>
-          <div className="create-account">
-              <Link to="/create">Create Account</Link>
-          </div>
+                    <button type="submit">Login</button>
+                </form>
+                <div className="create-account">
+                    <Link to="/create">Create Account</Link>
+                </div>
+
+            </div>
+        </div>
+
 
       </div>
     );
