a5d3fce28d6667ad727d00b4808cb921cccf806e
Aaron <10297223+aarondevon@users.noreply.github.com>
2020-08-24 09:48:35 -0500

    add styles to create account component

--- src/components/CreateAccount.tsx
+++ src/components/CreateAccount.tsx
@@ -5,15 +5,37 @@ export default class CreateAccount extends React.Component<{},{}> {
 
     render() {
         return (
-            <div className="container">
+            <div>
                 <Header />
-                <form className="form flex">
-                    <label id="email">Email</label>
-                    <input type="email" />
-                    <input type="password" />
-                    <input type="password" />
-                    <button type="submit">Create Account</button>
-                </form>
+                <div className="create-account-container">
+
+                    <div className="form-container">
+
+                        <form id="form" className=" form">
+                            <h2 className="create-account-header">Register With Us</h2>
+                            <div className="form-control">
+                                <label htmlFor="email">Email</label>
+                                <input id="email" type="email" value="Enter email"/>
+                            </div>
+
+                            <div className="form-control">
+                                <label htmlFor="passwordOne">Enter Password</label>
+                                <input type="password" />
+                            </div>
+
+                            <div className="form-control">
+                                <label htmlFor="passwordOne">Confirm Password</label>
+                                <input type="password" />
+                            </div>
+
+
+
+                            <button type="submit">Create Account</button>
+                        </form>
+                    </div>
+                </div>
+
+
             </div>
         )
     }
