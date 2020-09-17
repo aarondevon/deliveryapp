50923990f6a21aaf301ce3fce4e8a4132c32c562
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-09-17 12:45:13 -0500

    create server for api

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+// const pool = require('./db');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+
+
+//middleware
+app.use(cors());
+app.use(express.json()); // => allows us to access the req.body
+
+//remove when ready to deploy to heroku
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+
+if (process.env.NODE_ENV === 'production') {
+  //server static content
+  //yarn run build
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is starting on port ${PORT}`);
+});
