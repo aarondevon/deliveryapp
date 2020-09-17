052be1afba329b58767b35f7637d1ce930004b43
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-09-17 14:13:15 -0500

    add app.get and send index.html

--- index.js
+++ index.js
@@ -12,14 +12,18 @@ app.use(cors());
 app.use(express.json()); // => allows us to access the req.body
 
 //remove when ready to deploy to heroku
-app.use(express.static(path.join(__dirname, './client/build')));
+// app.use(express.static(path.join(__dirname, './client/build')));
 
 
 if (process.env.NODE_ENV === 'production') {
   //server static content
   //yarn run build
-  app.use(express.static(path.join(__dirname, './client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is starting on port ${PORT}`);
