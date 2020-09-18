ce238838572b9a5aacf446d049250ef9d47bc3bc
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-09-18 08:23:03 -0500

    test adding user to local DB

--- server.js
+++ server.js
@@ -14,16 +14,34 @@ app.use(express.json()); // => allows us to access the req.body
 //remove when ready to deploy to heroku
 // app.use(express.static(path.join(__dirname, './client/build')));
 
+//ROUTES//
 
-if (process.env.NODE_ENV === 'production') {
-  //server static content
-  //yarn run build
-  app.use(express.static(path.join(__dirname, 'client/build')));
-}
+//create user
+app.post("/create", async(req, res) => {
+  try {
+    const { userName, email, password } = req.body;
+    console.log('trying');
+    const newUser = await pool.query(
+        "INSERT INTO account (account_user_name, account_user_email, account_user_password)" +
+        "VALUES($1, $2, $3)",
+        [userName, email, password]
+    );
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
+    res.json(newUser);
+  } catch (err) {
+    console.error(err.message);
+  }
+})
+
+// if (process.env.NODE_ENV === 'production') {
+//   //server static content
+//   //yarn run build
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+// }
+//
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client/build/index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is starting on port ${PORT}`);
