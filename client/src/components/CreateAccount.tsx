1a48bda7381323356d099881f51afaaa4b6d9bcc
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-09-21 11:02:28 -0500

    update from, and set up component state

--- client/src/components/CreateAccount.tsx
+++ client/src/components/CreateAccount.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './Header'
+import { connect } from 'react-redux';
+import { register } from '../actions/auth';
 
-export default class CreateAccount extends React.Component<{},{}> {
+interface CreateAccountState  {
+    name: '',
+    email: '',
+    password: '',
+    passwordTwo: ''
+}
+
+class CreateAccount extends React.Component<any,CreateAccountState> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            passwordTwo: ''
+        }
+    }
+
+    onChange(event: any) {
+        this.setState({
+            ...this.state,
+            [event.target.name]: event.target.value
+        })
+    }
 
     render() {
+        const {name, email, password, passwordTwo} = this.state;
+
         return (
             <div>
                 <Header />
@@ -14,18 +43,44 @@ export default class CreateAccount extends React.Component<{},{}> {
                         <form id="form" className=" form">
                             <h2 className="create-account-header">Register With Us</h2>
                             <div className="form-control">
+                                <label htmlFor="name">Name</label>
+                                <input
+                                    id="name"
+                                    type="text"
+                                    placeholder="Name"
+                                    name="name"
+                                    value={name}
+                                    onChange={(event:any) => this.onChange(event)}
+                                    required
+                                />
+
+                            </div>
+
+                            <div className="form-control">
                                 <label htmlFor="email">Email</label>
-                                <input id="email" type="email" value="Enter email"/>
+                                <input
+                                    id="email"
+                                    type="email"
+                                    placeholder="Email"
+                                    name="email"
+                                    value={email}/>
                             </div>
 
                             <div className="form-control">
                                 <label htmlFor="passwordOne">Enter Password</label>
-                                <input type="password" />
+                                <input
+                                    type="password"
+                                    placeholder="Password"
+                                    name="password"
+                                    value={password}
+                                    // minLength="6"
+                                />
+
                             </div>
 
                             <div className="form-control">
-                                <label htmlFor="passwordOne">Confirm Password</label>
-                                <input type="password" />
+                                <label htmlFor="passwordTwo">Confirm Password</label>
+                                <input type="password" placeholder="Password" name="passwordTwo" value={passwordTwo}/>
                             </div>
 
 
@@ -40,3 +95,5 @@ export default class CreateAccount extends React.Component<{},{}> {
         )
     }
 }
+export default CreateAccount;
+// export default connect(null, { register })(CreateAccount);
