4cfe62f4aaf5b4d87896cc4a7902da9df3f7775d
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-05-17 09:09:21 -0500

    Remove Project files

--- client/src/components/ViewShifts.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class ViewShifts extends React.Component {
-    render () {
-        return (
-            <div>Whatever U want</div>
-        )
-    }
-}
