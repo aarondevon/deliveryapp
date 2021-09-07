9599d4ecf530a8dde85fb2e3d9083a9c3189dde0
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-09-07 15:28:09 -0500

    Start from scratch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,50 +1,26 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from 'react-router-dom';
-import Login from './components/auth/Login';
-import Register from "./components/auth/Register";
-import SummaryPage from "./components/SummaryPage";
-import StartShift from './components/StartShift';
-import ShiftListPage from './components/ShiftListPage';
-// Redux
-import { Provider } from 'react-redux';
-import store from './store';
+import logo from './logo.svg';
+import './App.css';
 
-export default class App extends React.Component<{}, {}> {
-  render() {
-    // @ts-ignore
-      return (
-          <Provider store={store}>
-              <div className="App">
-                  <Router>
-                      <Switch>
-                          <Route path="/create">
-                              <Register />
-                          </Route>
-                          <Route path="/summary">
-                              <SummaryPage />
-                          </Route>
-                          <Route path="/start-shift">
-                              <StartShift />
-                          </Route>
-                          <Route path="/view-shifts">
-                              <ShiftListPage />
-                          </Route>
-                          <Route path="/" exact={true}>
-                              <header className="App-header">
-                                  <Login login="hello" />
-                              </header>
-                          </Route>
-                      </Switch>
-                  </Router>
-
-              </div>
-          </Provider>
-
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
--- routes/api/runs.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// @route     GET api/runs
-// @desc      Test Route
-// @access    Public
-router.get('/', (req, res) => res.send('Runs route'));
-
-module.exports = router;
